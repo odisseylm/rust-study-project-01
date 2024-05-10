14cd61feb2ce8dd27a4636ef43f0e211c628aa05
vmelnykov <Volodymyr.Melnykov@gmail.com>
2024-05-10 03:36:24 +0300

    Adding amount type.

--- src/entities/amount.rs
+++ src/entities/amount.rs
@@ -69,10 +69,6 @@ fn parse_amount(s: &str) -> Result<Amount, ParseAmountError> {
     let s = s.trim();
     let last_space_bytes_offset_opt: Option<usize> = s.rfind(|ch: char|{ ch.is_ascii_whitespace() });
 
-    // IMPL with matches:
-    //   ++ no unsafe 'unwrap'
-    //   +  less-more short, but with IFs a bit shorter
-    //
     match last_space_bytes_offset_opt {
         None => { Err(ParseAmountError::NoCurrencyError) }
         Some(last_space_bytes_offset) => {
@@ -92,55 +88,6 @@ fn parse_amount(s: &str) -> Result<Amount, ParseAmountError> {
             }
         }
     }
-
-    /*
-    // IMPL with functional approach:
-    //   -- does NOT work now -> no flat_map
-    //
-    let res1: Result<usize, ParseAmountError> = last_space_bytes_offset.ok_or(ParseAmountError::NoCurrencyError);
-
-    // there is no flat_map ??!!
-    return res1.map(|last_space_bytes_offset: usize| {
-        let amount_and_cur = s.split_at(last_space_bytes_offset);
-        let str_amount = amount_and_cur.0.trim_end();
-        let str_cur = amount_and_cur.1.trim_start();
-
-        let cur_res = Currency::from_str(str_cur);
-        if cur_res.is_err() {
-            Err(ParseAmountError::ParseCurrencyError)
-        } else {
-            let amount_res = BigDecimal::from_str(str_amount);
-            if cur_res.is_err() {
-                Err(ParseAmountError::ParseAmountError(amount_res.err().unwrap()))
-            } else {
-                Ok(Amount::new(amount_res.unwrap(), cur_res.unwrap()))
-            }
-        }
-    }).unwrap()
-    */
-
-    /*
-    // IMPL with IFs and unwraps:
-    //   ++ simple
-    //   -- using unwrap()
-    //
-    if last_space_bytes_offset.is_none() {
-        return Err(ParseAmountError::NoCurrencyError)
-    }
-
-    let amount_and_cur = s.split_at(last_space_bytes_offset.unwrap());
-    let str_amount = amount_and_cur.0.trim_end();
-    let str_cur = amount_and_cur.1.trim_start();
-
-    let cur_res = Currency::from_str(str_cur);
-    if cur_res.is_err() { return Err(ParseAmountError::ParseCurrencyError) }
-
-    let amount_res = BigDecimal::from_str(str_amount);
-    if cur_res.is_err() { return Err(ParseAmountError::ParseAmountError(amount_res.err().unwrap())); }
-
-    // return Err(ParseAmountError::ParseCurrencyError);
-    return Ok(Amount::new(amount_res.unwrap(), cur_res.unwrap()));
-    */
 }
 
 
