c7a058243b699c6ae06e9ae4465307270a80e735
vmelnykov <Volodymyr.Melnykov@gmail.com>
2024-05-19 04:57:47 +0300

    Inheriting my error backtrace => code improvements => adding macros => 1st part (without generating for variants)

---
+++ other01/tests/macro1_test.rs
@@ -0,0 +1,24 @@
+
+
+
+// use tlborm_proc::TlbormDerive;
+// use crate::macro1::TlbormDerive;
+
+/*
+#[derive(TlbormDerive)]
+struct Foo {
+    #[tlborm_helper]
+    field: u32
+}
+
+#[derive(TlbormDerive)]
+enum Bar {
+    #[tlborm_helper]
+    Variant { #[tlborm_helper] field: u32 }
+}
+
+#[test]
+fn aaa() {
+
+}
+*/
---
+++ tests/my_deriv_macro_test.rs
@@ -0,0 +1,30 @@
+use other01::{HelloMacro, TlbormDerive};
+
+#[derive(TlbormDerive)]
+struct Foo {
+    #[tlborm_helper]
+    field: u32
+}
+
+#[derive(TlbormDerive)]
+enum Bar {
+    #[tlborm_helper]
+    Variant { #[tlborm_helper] field: u32 }
+}
+
+
+trait HelloMacro {
+    fn hello_macro();
+}
+
+
+#[derive(HelloMacro)]
+struct SS2 {
+    f: i32,
+}
+
+#[test]
+fn aaa() {
+
+}
+
