2cd3376cf4b1ad54f4c9fa6cc1a832f4d62ce45a
volodymyr <Volodymyr.Melnykov@gmail.com>
2024-07-16 01:42:09 +0300

    Refactoring => Using root as workspace only => removing '_subpackages' dir

--- account_soa/tests/macro1_test.rs
+++ account_soa/tests/macro1_test.rs
@@ -3,7 +3,7 @@
 // Seems tests are not launched automatically from' macro' subproject (at least under Idea).
 // As quick fix it is included there to be picked up.
 //
-// include!("./../_subpackages/static_error_macro/tests/macro1_test.rs");
+// include!("./../static_error_macro/tests/macro1_test.rs");
 
 // #[test]
 // fn to_have_idea_run_tests_menu_on_this_file() { assert_eq!(1, 1); }
