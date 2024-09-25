4b653a3bc17a34a8a56660dc62f8a11ab6c96044
volodymyr <Volodymyr.Melnykov@gmail.com>
2024-09-26 02:41:41 +0300

    Todo fix: using combinators instead of 'manual'

--- common/src/server_conf.rs
+++ common/src/server_conf.rs
@@ -1,4 +1,5 @@
 use anyhow::anyhow;
+use itertools::Itertools;
 use crate::env::{env_var_static, env_var};
 use crate::net::ConnectionType;
 use crate::obj_ext::ValExt;
@@ -7,26 +8,48 @@ use crate::obj_ext::ValExt;
 
 
 pub fn get_server_port(service_name: &str) -> Result<u16, anyhow::Error> {
+    let default_port: u16 = 8080;
+
+    /*
+    // Manual approach
+    use crate::env::EnvVarError;
+
     let precise_app_port_env_name_1 = format!("{}_SERVER_PORT", service_name.to_uppercase());
     let precise_app_port_env_name_2 = format!("{}SERVER_PORT", service_name.to_uppercase());
     let general_app_port_env_name_1 = "SERVER_PORT";
     let general_app_port_env_name_2 = "PORT";
-    let default_port: u16 = 8080;
 
-    // TODO: Use 'for' for it
     let port_env_1 = env_var(&precise_app_port_env_name_1) ?;
     let port_env_2 = env_var(&precise_app_port_env_name_2) ?;
     let port_env_alt_1 = env_var_static(general_app_port_env_name_1) ?;
     let port_env_alt_2 = env_var_static(general_app_port_env_name_2) ?;
 
     let str_port = port_env_1.or(port_env_2).or(port_env_alt_1).or(port_env_alt_2);
+    */
+
+    let port_env_vars = [
+        &format!("{}_SERVER_PORT", service_name.to_uppercase()),
+        &format!("{}SERVER_PORT", service_name.to_uppercase()),
+        "SERVER_PORT",
+        "PORT",
+    ];
+
+    let str_port = port_env_vars.iter()
+        .map(|env_name| env_var(env_name))
+        .find_or_first(|res|{
+            let res = res.as_ref();
+            // skip 'Ok(None) => env var not found'
+            res.is_err() || res.is_ok_and(Option::is_some)
+        })
+        .unwrap_or(Ok(None)) ?
+        ;
 
     match str_port {
         None => Ok(default_port),
         Some(ref str_port) => {
             use core::str::FromStr;
             let port: u16 = FromStr::from_str(str_port)
-                .map_err(|_|anyhow!("SERVER_PORT value [{str_port}] has wrong format.")) ?;
+                .map_err(|_|anyhow!("XXX_SERVER_PORT value [{str_port}] has wrong format.")) ?;
             Ok(port)
         }
     }
